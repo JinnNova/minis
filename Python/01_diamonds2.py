bc6367d53b61644a2ea968c2a2aaf6da3c662679
Jinn <jozie@mbnet.fi>
2017-09-28 18:51:03 +0300

    Week 2: drawing given amount of ASCII diamonds

---
+++ Python/01_diamonds2.py
@@ -0,0 +1,33 @@
+# Not so useful Python minis by Hanna "Jinn" Enqvist
+
+def main():
+    h = input("Give an odd number (height of the diamond): ")
+    while h:
+        h = int(h)
+        n = input("Give a number of diamonds (1-10): ")
+        n = int(n)
+        s = str(input("Give a character: "))
+
+        drawDiamond(h,s,n)
+
+        print()
+        h = input("Give an odd number, or press enter to quit: ")
+
+def drawDiamond(h,s,n):
+    rivit = [s]
+    for x in range(1,h,2):
+        rivit.append(s + " " * x + s)
+
+    rivit += rivit[:-1][::-1]
+
+    for rivi in rivit:
+        for y in range (0,n):
+            if y < n-1:
+                # Easier way to center stuff in Python
+                print(rivi.center(h," "), end=" ")
+            else:
+                print(rivi.center(h," "))
+
+    return
+
+main()

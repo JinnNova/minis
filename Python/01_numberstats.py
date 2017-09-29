221e7f8ddd12d1ae0e54fe96e193a124fe222680
Jinn <jozie@mbnet.fi>
2017-09-29 21:00:42 +0300

    Week 1: A Program to calculate information about given numbers

---
+++ Python/01_numberstats.py
@@ -0,0 +1,27 @@
+#program to find and calculate min max sum and mean of the given input numbers
+summ = 0.0
+minn = 0.0
+maxx = 0.0
+counter = 0
+mean = 0.0
+
+line = input()
+while line:
+    line = float(line)
+
+    if line > maxx:
+        maxx = line
+    #cant be elif or else. if user inputs only one number then min and max are the same
+    if line < minn or minn == 0.0:
+        minn = line
+
+    summ = summ + line
+    counter = counter + 1
+
+    line = input()
+
+#if user does not input any numbers, we dont want to divide by 0
+if counter != 0:
+    mean = summ / counter
+
+print("Minimum {0}, maximum {1}, sum {2} and mean {3}".format(minn, maxx, summ, mean))

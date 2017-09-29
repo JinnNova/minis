7efa1d4b9b4be5480febd207408e95b6ffcd8674
Jinn <jozie@mbnet.fi>
2017-09-29 20:53:39 +0300

    Week 1: Printing given amount of prime numbers

--- Python/01_primes.py
+++ Python/01_primes.py
@@ -1,17 +1,21 @@
+# Not so useful Python minis by Hanna "Jinn" Enqvist
+
 def main():
-    n = int(input())
-    k = int(input())
+    n = int(input("Print primes up to number X (Give a whole number X): "))
+    k = int(input("Print every N:th prime number. (Give N): "))
     vastaus = []
 
     for x in range(0,n):
         if isPrime(x):
             vastaus.append(x)
-
+            
+    # Splitting the list of primes
     vastaus = vastaus[0::k]
     for x in vastaus:
         print(x, end=' ')
     print()
 
+# checking if a number is Prime
 def isPrime(i):
     if i < 2:
         return False
